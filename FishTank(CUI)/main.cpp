064a5edb5888e7f46fa8d8a1241333628edc1502
Ianking Penn <cosineufo@gmail.com>
2012-12-21 12:59:21 +0800

    fishtank

--- FishTank(CUI)/main.cpp
+++ FishTank(CUI)/main.cpp
@@ -1,57 +1,16 @@
 #include "env.h"
-#include "TAAI.h"
-#include "st34.h"
-#include "st20.h"
-#include "st31.h"
-#include "st32.h"
-#include "st35.h"
-#include "st26.h"
-#include "st26(sp).h"
-#include "st06.h"
+#include "st01.h"
+#include "st02.h"
+#include "st03.h"
 
 env& fish::host = env::GetInstance();
 
 int main()
 {
     env& system = env::GetInstance();
-    system.addAI(new st34());
-    system.addAI(new st31());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
-    system.addAI(new st32());
-    system.addAI(new st261());
-    system.addAI(new st20());
-    system.addAI(new st35());
-    system.addAI(new st26());
-    system.addAI(new st06());
+    system.addAI(new st01());
+    system.addAI(new st02());
+    system.addAI(new st03());
     system.play();
 }
 
